71262b51c290638d22264c2f7c09cd01cc23598f
VQuine <01010110.v@gmail.com>
2021-06-23 18:50:55 -0400

    commit tree a5ada12faf7620f12faae25864c7aab654fc6cf8

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+class Cat {
+    // C O N S T R U C T O R allows for usage of ARGUMENTS with 'new'
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+    // I N S T A N C E   METHOD
+    speak() {
+        return `${this.name} says meow!`
+    }
+}
+
+class Dog {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+
+    speak() {
+        return `${this.name} says woof!`
+    }
+}
+
+class Bird {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+    //  C O N D I T I O N A L   OUTPUT
+    speak() {
+        // == make the datatypes the same - compare
+        if (this.sex == 'male') {
+            return `It's me! ${this.name}, the parrot!`
+        } else {
+            return `${this.name} says squawk!`
+        }
+    }
+}
